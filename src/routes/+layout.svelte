1393c628b78fcb281967d7f3bd74af4cb5f854a7
lipusmah <lipusblaz@gmail.com>
2023-08-11 16:36:41 +0200

    https api

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import "../app.scss";
     import Header from "$lib/components/Header.svelte";
 	import { OpenAPI } from "$lib/api";
-    OpenAPI.BASE = "http://elevation.lipusblaz.com";
+    OpenAPI.BASE = "https://elevation.lipusblaz.com";
 </script>
 
 <div class="layout">
